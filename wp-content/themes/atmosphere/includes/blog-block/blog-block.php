c2c5a46149075a8fcee20d2458ec727cbbeeeafe
rademax <rademaxbh@gmail.com>
2018-08-22 00:31:24 +0300

    Designed playlists and blog posts, added test audios

--- wp-content/themes/atmosphere/includes/blog-block/blog-block.php
+++ wp-content/themes/atmosphere/includes/blog-block/blog-block.php
@@ -1,7 +1,18 @@
 <section class="blog-block">
   <div class="blog-block__inner">
-
-    {-- Блог выводить сюда  --}}
-
+    <div class="blog-items">
+      <?php $query = new WP_Query(['category_name' => 'blog']); ?>
+      <?php if($query->have_posts()): ?>
+        <?php while($query->have_posts()): ?>
+          <div class="blog-item">
+            <?php $query->the_post(); ?>
+            <div class="blog-item__title"><?php the_title(); ?></div>
+            <p class="blog-item__description"><?php the_content(); ?></p>
+            <span class="blog-item_date"><?= get_the_date(); ?></span>
+          </div>
+          <br>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
   </div>
 </section>
--- wp-content/themes/atmosphere/portfolio.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-<?php get_template_part('includes/portfolio-work/portfolio-work'); ?>
-
-<?php get_footer(); ?>
