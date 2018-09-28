018ab69c5f7dfd2fa3626974d8aa6276de75c60b
SergeyPopenko <skino4ek@ukr.net>
2018-09-28 14:58:54 +0300

    fin desktop

--- src/_blocks/blog-block/blog-block.php
+++ src/_blocks/blog-block/blog-block.php
@@ -1,24 +1,23 @@
-<section class="blog-block" data-blog-section>
-  <h2 class="visually-hidden">Blog for atmospheric music"</h2>
-  <div class="blog-block__container">
-    <div class="blog-block__inner">
-      <div class="blog-block__items">
-        <?php $query = new WP_Query(['category_name' => 'blog']); ?>
-        <?php if($query->have_posts()): ?>
-          <?php while($query->have_posts()): ?>
-            <div class="blog-block__item">
-              <?php $query->the_post(); ?>
-              <h3 class="blog-block__item-title"><?php the_title(); ?></h3>
-              <div class="blog-block__item-description"><?php the_content(); ?></div>
-              <time class="blog-block__item-date"><i> <?= get_the_date(); ?></i></time>
-            </div>
-          <?php endwhile; ?>
-        <?php endif; ?>
+<section class="blog-block wrap" data-blog-section>
+  <div class="blog-block__wrap">
+    <h2 class="visually-hidden">Blog for atmospheric music"</h2>
+    <div class="blog-block__container">
+      <div class="blog-block__inner">
+        <div class="blog-block__items">
+          <?php $query = new WP_Query(['category_name' => 'blog']); ?>
+          <?php if($query->have_posts()): ?>
+            <?php while($query->have_posts()): ?>
+              <div class="blog-block__item">
+                <?php $query->the_post(); ?>
+                <h3 class="blog-block__item-title"><?php the_title(); ?></h3>
+                <div class="blog-block__item-description"><?php the_content(); ?></div>
+                <time class="blog-block__item-date"><i> <?= get_the_date(); ?></i></time>
+              </div>
+            <?php endwhile; ?>
+          <?php endif; ?>
+        </div>
       </div>
     </div>
   </div>
 </section>
-
-
-
 
--- wp-content/themes/atmosphere/includes/footer/footer.php
+++ wp-content/themes/atmosphere/includes/footer/footer.php
@@ -2,11 +2,10 @@
   <div class="wrap">
     <div class="footer__wrap">
       <div class="footer__privacy">
-        <p><small>© </small><span id="year-current">2018</span> Immobiz Immobilien GmbH.</p> <p>All rights reserved</p>
-        <p><a href="/wp-content/themes/atmosphere/img/file/privacy.pdf" title="Group Privacy Policy">Group Privacy Statement</a></p>
+        <p><small>© </small><span id="year-current">2018</span> Immobiz Immobilien GmbH.</p> <p>All rights reserved.&nbsp;
+        <a href="/wp-content/themes/atmosphere/img/file/privacy.pdf" title="Group Privacy Policy">Group Privacy Statement</a></p>
       </div>
       <div class="footer__contact">
-        <p>Contact us:</p>
         <p>Mail us: <a href="mailto:office@schwarcz.net ">office@schwarcz.net</a></p>
         <p>Phone us: <a href="tel:+4315133700">+4315133700</a></p>
       </div>
